e7f4c57e4fa6d9565fad7e21b08a201855e300ad
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2018-07-16 11:51:53 +0100

    Uplifted using giter8 (#1)
    * Uplifted using giter8
    
    * Added audit stub
    
    * Added audit stub

--- app/v2/outcomes/MtdIdLookupOutcome.scala
+++ app/v2/outcomes/MtdIdLookupOutcome.scala
@@ -14,12 +14,18 @@
  * limitations under the License.
  */
 
-import sbt.{ForkOptions, TestDefinition}
-import sbt.Tests.{Group, SubProcess}
+package v2.outcomes
 
-object TestPhases {
-  def oneForkedJvmPerTest(tests: Seq[TestDefinition]): Seq[Group] =
-    tests map {
-      test => Group(test.name, Seq(test), SubProcess(ForkOptions(runJVMOptions = Seq("-Dtest.name=" + test.name, "-Dlogger.resource=logback-test.xml"))))
-    }
+import v2.models.errors.MtdError
+
+object MtdIdLookupOutcome {
+
+  type MtdIdLookupOutcome = Either[MtdError, String]
+
+  sealed trait MtdIdLookupError extends MtdError
+
+  object NotAuthorised extends MtdIdLookupError
+
+  object DownstreamError extends MtdIdLookupError
+
 }
