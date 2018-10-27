a2b45473eabf820ccdaea3d2cbb1c213de9b6efd
mac grewal <mac.grewal@hotmail.com>
2018-10-27 12:03:58 +0100

    Passing header carrier to audit event (#40)

--- app/v2/services/EopsDeclarationService.scala
+++ app/v2/services/EopsDeclarationService.scala
@@ -68,6 +68,7 @@ class EopsDeclarationService @Inject()(auditService: AuditService, connector: De
   private def auditSuccessfulSubmission(submission: EopsDeclarationSubmission,
                                         correlationId: String)
                                        (implicit ec: ExecutionContext,
+                                        hc: HeaderCarrier,
                                         userDetails: UserDetails): Future[AuditResult] = {
     val details = EopsDeclarationAuditDetail(
       userDetails.userType,
