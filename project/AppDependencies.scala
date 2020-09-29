8fc9f9e2dfc38c165ae3022899cd75b0d8faae3c
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-09-29 14:29:57 +0100

    MTDSA-7177

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -24,20 +24,20 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26" % "1.8.0",
     "uk.gov.hmrc" %% "domain" % "5.9.0-play-26",
-    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.4.4" cross CrossVersion.full),
-    "com.github.ghik" % "silencer-lib" % "1.4.4" % Provided cross CrossVersion.full
+    compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.6.0" cross CrossVersion.full),
+    "com.github.ghik" % "silencer-lib" % "1.6.0" % Provided cross CrossVersion.full
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
-    "org.scalatest" %% "scalatest" % "3.1.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.2.0" % scope,
     "com.vladsch.flexmark" % "flexmark-all" % "0.35.10" % scope,
-    "org.scalamock" %% "scalamock" % "4.4.0" % scope,
+    "org.scalamock" %% "scalamock" % "5.0.0" % scope,
 
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
 
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % scope,
 
-    "com.github.tomakehurst" % "wiremock" % "2.25.1" % scope
+    "com.github.tomakehurst" % "wiremock" % "2.27.2" % scope
   )
 
   // Fixes a transitive dependency clash between wiremock and scalatestplus-play
