9f87a51ab72219674f7a04f2c4edafb9bdd206c8
Thepworth95 <56401451+Thepworth95@users.noreply.github.com>
2021-06-28 09:06:11 +0100

    address comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,7 +22,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.3.0",
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.4.0",
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
